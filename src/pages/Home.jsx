e1be577b90c41d723483b9434c4c6aad81c20a43
SaorCampos <saor93@gmail.com>
2022-10-18 22:32:30 +0000

    adicionado os componentes navbar e card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components/PokemonCard";
 
 export const Home = () => {
     return(
         <React.Fragment>
-            Home
+            <Navbar/>
+            <PokemonCard/>
         </React.Fragment>
     )
 }
